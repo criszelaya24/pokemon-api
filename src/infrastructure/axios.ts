2f6bdb7032fc08270693abf4d3d686ede9131b6f
cristopher Palacios <criszelaya24@hotmail.com>
2022-09-07 12:01:36 -0600

    Adding class axios to extends to classes

---
+++ src/infrastructure/axios.ts
@@ -0,0 +1,37 @@
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
+
+declare module 'axios' {
+    // eslint-disable-next-line @typescript-eslint/no-empty-interface
+    interface AxiosResponse<T = any> extends Promise<T> {}
+}
+
+export default class Axios {
+
+    protected readonly instance: AxiosInstance;
+
+    constructor(baseURL: string) {
+        this.instance = axios.create({
+            baseURL,
+        });
+
+        this._initializeResponseInterceptor();
+    }
+
+    private _initializeResponseInterceptor = () => {
+        this.instance?.interceptors.response.use(
+            this._handleResponse,
+            this._handleError,
+        );
+    };
+
+    private _handleResponse = ({ data }: AxiosResponse) => data;
+
+    protected _handleError = (error: any) => Promise.reject(error);
+
+    protected async get<T>(url: string, config?: any): Promise<T> {
+        const response = await this.instance.get(url, { ...config });
+
+        return response as T;
+    }
+
+}
